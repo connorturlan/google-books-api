f82cd2e9b9b19ce3f2b9c5e9d06b65d2dbeb7a79
Connor <connorturlan@gmail.com>
2022-07-18 08:55:08 +0930

    separated search styles

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/style.css";
-import { API_KEY } from "./api";
+import { API_KEY } from "./api.js";
 
 function searchAPI(query) {}
 
-class Results extends React.Component {}
+class Results extends React.Component {
+	render() {
+		return <div></div>;
+	}
+}
 
-const search = ReactDOM.createRoot(document.getElementById("results"));
-search.render(<Results />);
+// get the results container.
+const results = ReactDOM.createRoot(document.getElementById("results"));
+
+// attach the results.render to whenever the search button is pressed.
+let search = document.querySelector(".search");
+search.addEventListener("submit", (event) => {
+	event.preventDefault();
+
+	console.log("lmao, begin search.");
+	results.render(<Results />);
+});
