083eeec29620a5d542f0553a47cbc2b9773bb239
Connor <connorturlan@gmail.com>
2022-07-21 11:16:23 +0930

    increased book limits

--- src/api.js
+++ src/api.js
@@ -16,7 +16,7 @@ export async function searchAPI(query) {
 	const response = await fetch(
 		`https://www.googleapis.com/books/v1/volumes?q=${encodeURI(
 			query
-		)}&key=${API_KEY}`
+		)}&maxResults=40&key=${API_KEY}`
 	);
 
 	if (response.ok) {
