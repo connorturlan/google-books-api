5ded72a5cb216bb72d0699bebe22c93f3ddd5f9f
Connor <connorturlan@gmail.com>
2022-07-21 10:55:50 +0930

    fixed searchbar floating too high on load

--- src/results.js
+++ src/results.js
@@ -3,6 +3,16 @@ import { getBookCards } from "./books.js";
 // get the results container.
 const results = document.getElementById("results");
 
+// show the results pane.
+export function show() {
+	results.classList.remove("results--hidden");
+}
+
+// hide the results pane.
+export function hide() {
+	results.classList.add("results--hidden");
+}
+
 // clear all book search results.
 export function clear() {
 	// remove all children from the results grid.
@@ -14,11 +24,13 @@ export function clear() {
 
 // update the results section of the page with given entries of books.
 export function update(books) {
+	hide();
 	clear();
 
 	// process all the books into cards and append them to the results.
 	if (books.length > 0) {
 		const cards = getBookCards(books);
 		cards.reduce((n, card) => results.appendChild(card), null);
+		show();
 	}
 }
